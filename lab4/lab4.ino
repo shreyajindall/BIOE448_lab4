d4abde493dbbf2b305bea8720303aedd8ebf4320
shreyajindall <sj60@rice.edu>
2024-02-06 15:12:38 -0600

    declared variables and setup

--- lab4/lab4.ino
+++ lab4/lab4.ino
@@ -1,6 +1,13 @@
+const int trigPin = 11;
+const int echoPin = 12;
+long duration;
+int distanceCm, distanceInch;
+
 void setup() {
   // put your setup code here, to run once:
-
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);
+Serial.begin(9600);
 }
 
 void loop() {
